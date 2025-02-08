f7277cef3aca1e5e90eee43fbe01c96792ae8838
Abduraahman19 <abdurrahmanmughal0303@gmail.com>
2025-02-08 14:29:10 +0500

    update

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+     useUnifiedTopology: true,
+    });
+    console.log('Database connected successfully');
+  } catch (err) {
+    console.error('Database connection failed:', err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
