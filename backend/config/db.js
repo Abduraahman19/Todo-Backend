40052979829b52832075552bed89d0b7549dcf9e
Abduraahman19 <abdurrahmanmughal0303@gmail.com>
2025-02-10 15:49:36 +0500

    Updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-     useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
     console.log('Database connected successfully');
   } catch (err) {
